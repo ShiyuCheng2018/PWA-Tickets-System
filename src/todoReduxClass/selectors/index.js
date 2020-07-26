3472c08ef8eae97ecca98d2a33d1aa309ebd3647
Shiyu Cheng <starkeyshiyu@gmail.com>
2020-07-25 23:19:55 -0700

    reselect

--- src/todoReduxClass/selectors/index.js
+++ src/todoReduxClass/selectors/index.js
@@ -1,16 +1,20 @@
+import {createSelector} from "reselect";
+
 export const getText = (state) => state.get("text");
 export const getFilter = (state) =>  state.get("filter");
 
-export const getVisibleTodos = (state) => {
-    const data = state.getIn(["todos", "data"]);
-    const filter = state.get("filter");
+const getTodos = state => state.todos.data;
 
-    switch (filter) {
-        case "ALL": return data
-        case "COMPLETED":
-            return data.filter(item=> item.get("completed"))
-        case "ACTIVE":
-            return data.filter(item => !item.get("completed"))
-        default: return new Error("Unknown filter " + filter)
+export const getVisibleTodos = createSelector(
+    [getTodos, getFilter],
+    (todos, filter) => {
+        switch (filter) {
+            case "ALL": return todos
+            case "COMPLETED":
+                return todos.filter(item=> item.get("completed"))
+            case "ACTIVE":
+                return todos.filter(item => !item.get("completed"))
+            default: return new Error("Unknown filter " + filter)
+        }
     }
-}
+)
